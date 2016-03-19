0037ca20ca52e69d551d39823a3b3f53e7d799c2
billumandal <billu.mandal@gmail.com>
2016-03-19 11:33:20 +0530

    2nd commit

--- poll/settings.py
+++ poll/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'sx%gh(l86l$0dx0jukfslc054vo8r5+h7kww&rjj-0!^q1n-@y'
+SECRET_KEY = 'billu123'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'poll.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'polldatabase.db'),
     }
 }
 
